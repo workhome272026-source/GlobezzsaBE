9fa2418c81f934cc4a5d1bf27c6c84516bc8e968
workhome272026-source <workhome272026@gmail.com>
2026-08-27 16:50:34 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/globessza/globessza/GlobesszaApplication.java
@@ -0,0 +1,13 @@
+package com.globessza.globessza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobesszaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobesszaApplication.class, args);
+	}
+
+}
